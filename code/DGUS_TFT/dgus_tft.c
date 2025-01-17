b1a1ebfd1393fc5a48481fd7348929c62b95e219
thehumbleboy <nj60168#gmail.com>
2025-01-17 17:17:29 +0530

    added new branch, refined the dgus_tft.c file a little bit

--- code/DGUS_TFT/dgus_tft.c
+++ code/DGUS_TFT/dgus_tft.c
@@ -1,17 +1,17 @@
 #include "dgus_tft.h"
 
 DGUS_Parameters gauge_high,gauge_low;
+uint32_t DWIN_Buffer [8] = {0x5a,0xa5,0x05,0x82,0x10,0x00,0x00,0x00};
 
 void Display_Speedometer_Value(DGUS_Parameters *gauge_value){
 
     gauge_high.temp = gauge_value->Uart_Speedometer & 0XFF00 >> 8;
     gauge_low.temp = gauge_value->Uart_Speedometer & 0X00FF;
-    Uart_SendValue(0x5a); // Header
-    Uart_SendValue(0xa5); // header
-    Uart_SendValue(0x05); // Number of bytes being sent
-    Uart_SendValue(0x82); // Send/Set VP
-    Uart_SendValue(0x10); // address
-    Uart_SendValue(0x00); // address
-    Uart_SendValue(0X00); 
-    Uart_SendValue_Dgus(&gauge_low); // low 2 bytes value
+    for(int i = 0;i<8;i++){
+        Uart_SendValue(DWIN_Buffer[i]);
+        if(i == 7){
+            DWIN_Buffer[i] = gauge_low.temp;
+            Uart_SendValue(DWIN_Buffer[i]);
+        }
+    }
 }
